30785a738c7ce65a480e627e489d53edf486812f
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-07-02 00:40:55 +0100

    colour

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById("main").style.backgroundColor = "antiquewhite"
+
 function random() {
     let random = Math.floor(Math.random() * 5)
     let img = document.getElementById("img")
@@ -5,15 +7,19 @@ function random() {
 
     if (random == 1) {
         img.src = "catA.jpg"
-        txt.innerHTML = "Food is food! <br> <p><i>-Cat A<i><p>"
+        txt.innerHTML = "Food is food! <br> <i>-Cat A</i>"
+        document.getElementById("main").style.backgroundColor = "antiquewhite"
     } else if (random == 2) {
         img.src = "catB.jpg"
-        txt.innerHTML = "When you drink water, you drink water! <br> <p><i>-Cat B<i><p>"
+        txt.innerHTML = "When you drink water, you drink water! <br> <i>-Cat B</i>"
+        document.getElementById("main").style.backgroundColor = "#d6ffda"
     } else if (random == 3) {
         img.src = "catC.jpg"
-        txt.innerHTML = "Dancing for me is fun! <br> <p><i>-Cat C<i><p>"
+        txt.innerHTML = "Dancing for me is fun! <br> <i>-Cat C</i>"
+        document.getElementById("main").style.backgroundColor = "#c9cdff"
     } else if (random == 4) {
         img.src = "catD.jpg"
-        txt.innerHTML = "I sorta like gorillas, I mean c'mon! <br> <p><i>-Cat D<i><p>"
+        txt.innerHTML = "I sorta like gorillas, I mean c'mon! <br> <i>-Cat D</i>"
+        document.getElementById("main").style.backgroundColor = "#ddb0ff"
     } 
 }
